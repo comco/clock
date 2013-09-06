e97667cbc876d56f44fe0a0003a0665736fa7a90
Krasimir Georgiev <void.unsigned@gmail.com>
2013-09-06 16:43:56 +0300

    Added quil dependency and a simple screen.

--- src/clock/core.clj
+++ src/clock/core.clj
@@ -1,9 +1,37 @@
 (ns clock.core
+  (:require [quil.core :as qc])
   (:gen-class))
 
+(def birds [{:x 10 :y 20}
+            {:x 32.56 :y 38}
+            {:x 14 :y 24}])
+
+(def world {:birds birds})
+
+(defn draw []
+  (qc/background-float 0)
+  (qc/stroke-float 255)
+  (qc/stroke-weight 5)
+  (with-translation [(/ 200 2) (/ 200 2)]
+    (with-rotation [qc/QUARTER-PI]
+      (begin-shape :points)
+      (doseq [bird (world :birds)]
+        (vertex (bird :x) (bird :y)))
+      (end-shape)))
+  (display-filter :invert))
+
+(defn setup []
+  (smooth)
+  (no-stroke)
+  (fill 226)
+  (frame-rate 24))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (defsketch world-view
+    :title "Clock"
+    :setup setup
+    :draw draw
+    :size [800 600]))
